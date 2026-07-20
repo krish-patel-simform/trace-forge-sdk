a8d496c4956677e45731f84876a2b8dda5ab9e8e
krish-patel-simform <krish.patel@simformsolutions.com>
2026-07-20 17:15:34 +0530

    Implement the basic config file

---
+++ src/TraceForge.ts
@@ -0,0 +1,26 @@
+import type { TraceForgeConfig } from "./types/config.js";
+
+class TraceForgeSDK {
+  private config: TraceForgeConfig | null = null;
+
+  init(config: TraceForgeConfig) {
+    this.config = config;
+  }
+
+  getConfig() {
+    if (!this.config) {
+      throw new Error(
+        "TraceForge has not been initialized. Call TraceForge.init() first.",
+      );
+    }
+
+    return this.config;
+  }
+
+  isIntialized() {
+    if (this.config) return true;
+    return false;
+  }
+}
+
+export const TraceForge = new TraceForgeSDK();
